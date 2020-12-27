53be8fbf917947e9274014a83e1bdca0ced8e7d5
David Chen <david.chen@epita.fr>
2020-12-27 19:41:57 +0100

    model: 2d: Added checkpoint, testing on colab

--- models/2d/main.py
+++ models/2d/main.py
@@ -1,12 +1,9 @@
 import sys
-import os
-from random import shuffle
-import numpy as np
-from pathlib import Path
-import nibabel as nib
 from slices_generator import SlicesGenerator
 from construct_dataset import construct_slices
+from unet import get_model
 
+from keras.callbacks import ModelCheckpoint
 
 def main(path):
     # Path leads to the where the data is Ultrecht, Singapore etc...
@@ -14,7 +11,13 @@ def main(path):
     # Get train generator
     X, Y = construct_slices(path)
     # Do a train test split => Layer ?
-    slices_generator = SlicesGenerator(X, Y, batch_size=1)
-    # Do something
+    gen = SlicesGenerator(X, Y, batch_size=32)
+    model = get_model((572, 572), 3)
+    checkpoint = ModelCheckpoint("best_model.h5", save_best_only=True)
+
+    model.fit(
+        gen,
+        verbose=1,
+        callbacks=checkpoint)
 if __name__ == "__main__":
     main(sys.argv[1])
